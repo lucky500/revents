6c7a723860200ed7423a9ab894c7c1e17aeea94e
Luciano Oliveira <luciano101@msn.com>
2018-05-19 07:38:29 -0400

    remove create react app files and add npm-shrinkwrap.json

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/layout/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root');
+
+//we create a render method and assign it to a annonymous function
+let render = () => {
+    ReactDOM.render(<App />, rootEl);
+}
+
+//now lets query module replacement on webpack
+if(module.hot){
+    module.hot.accept('./app/layout/App', () =>{
+        //we use setTimeout so we can render immediately.
+        setTimeout(render);
+    })
+}
+
+render();
+
+
 registerServiceWorker();
