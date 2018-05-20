ad9cfa3afcfd555ca41441bf40f58a069e417639
Luciano Oliveira <luciano101@msn.com>
2018-05-20 05:44:14 -0400

    add semantic ui and simple grid, add EventDashboard Component

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Re-vents</h1>
+        <EventDashboard/>
       </div>
     );
   }
